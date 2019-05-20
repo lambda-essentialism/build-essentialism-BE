c1fc0d58a9b633853637b97fdd136fa7d9cc16e7
Garrett Weems <gwgraphicdesign@gmail.com>
2019-05-20 13:09:22 -0400

    init

---
+++ src/main/java/com/lambda/essentialism/EssentialismApplication.java
@@ -0,0 +1,14 @@
+package com.lambda.essentialism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssentialismApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EssentialismApplication.class, args);
+  }
+
+}
+
